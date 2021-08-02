6901c5531a806f2fe61f582a0415120c74430e0a
Sanyalak-T <isanyalakt@gmail.com>
2021-08-02 19:57:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+//using async/await
+async function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
+    jokeEl.innerHTML = data.joke
+}
+
+// using .then()
+// function generateJoke() {
+//     const config = { headers: { 'Accept': 'application/json' }}
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke
+//         })
+//     }
+
+// write fatch like basic
+//      function generateJoke() {
+//         fetch('https://icanhazdadjoke.com', headers: { 'Accept': 'application/json' })
+//         .then((res) => res.json())
+//         .then((data) => { jokeEl.innerHTML = data.joke })
+//      }
